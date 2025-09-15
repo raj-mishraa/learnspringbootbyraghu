9bd5fea63dd93e98cbdb05a90730694d2f02135f
raj-mishraa <mraj82931@gmail.com>
2025-09-15 18:05:43 +0530

    here i am using application properties

--- src/main/java/in/nareshit/raghu/Product.java
+++ src/main/java/in/nareshit/raghu/Product.java
@@ -1,15 +1,40 @@
 package in.nareshit.raghu;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
-
+//import lombok.Getter;
+//import lombok.Setter;
+@ConfigurationProperties(prefix="my.value")
 //ctrl+shift+O 
 @Component("pob")
+//@Setter
+//@Getter
 public class Product {
-	@Value("5")
+//	@Value("5")
 	private int pid;
-	@Value("PEN")
+public int getPid() {
+		return pid;
+	}
+
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+
+	public String getPcode() {
+		return pcode;
+	}
+
+
+	public void setPcode(String pcode) {
+		this.pcode = pcode;
+	}
+
+
+	//	@Value("PEN")
 	private String pcode;
+	
 	
 	public void display() {
 		System.out.println(pid + "-" + pcode);
